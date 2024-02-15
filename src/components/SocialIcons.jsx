47d234e91a9d3540507de1975152a4379506f906
Nakul <nakulbhat034@gmail.com>
2024-02-15 18:44:49 +0530

    added hover animations

--- src/components/SocialIcons.jsx
+++ src/components/SocialIcons.jsx
@@ -1,19 +1,33 @@
-import React from 'react'
-import instaicon from '../../static/igicon7.png'
-import linkedinicon from '../../static/linked7.png'
-import { motion } from 'framer-motion'
+import React from "react";
+import instaicon from "../../static/igicon7.png";
+import linkedinicon from "../../static/linked7.png";
+import { motion } from "framer-motion";
 
 const SocialIcons = () => {
   return (
     <motion.div
-    initial={{ opacity: 0 }}
-    animate={{ opacity: 1 }}
-    transition={{ duration: 0.5 }} 
-     className='fixed bottom-0 left-0 flex w-32 justify-evenly m-9'>
-      <motion.button><img src={instaicon} alt='logo' className='md:h-[55px] rounded-xl' /></motion.button>
-      <motion.button><img src={linkedinicon} alt='logo' className='md:h-[55px] rounded-xl' /></motion.button>
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ duration: 0.5 }}
+      className="fixed bottom-0 left-0 flex w-32 justify-evenly m-9"
+    >
+      <motion.button
+        whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 1.25, rotate: "2.5deg" }}
+        transition={{
+          duration: 0.5,
+          type: "spring",
+          stiffness: 260,
+          damping: 20,
+        }}
+      >
+        <img src={instaicon} alt="logo" className="md:h-[55px] rounded-xl" />
+      </motion.button>
+      <motion.button whileHover={{ scale: 1.1 }} whileTap={{ scale: 1.25 , rotate: "2.5deg"}}>
+        <img src={linkedinicon} alt="logo" className="md:h-[55px] rounded-xl" />
+      </motion.button>
     </motion.div>
-  )
-}
+  );
+};
 
-export default SocialIcons
+export default SocialIcons;
--- src/components/SplashScreen.jsx
+++ src/components/SplashScreen.jsx
@@ -2,7 +2,6 @@ import React from 'react'
 import splashphoto from "../../static/image2.png"
 import { motion } from 'framer-motion'
 
-
 const SplashScreen = () => {
   return (
     <motion.div
@@ -12,9 +11,9 @@ const SplashScreen = () => {
     >
       <h1 className='text-[64px] m-12 font-["josefin_sans"] font-semibold'>Be Unique</h1>
       <p className='text-4xl md:w-2/4 md:m-12 font-light' >Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-      <motion.button whileHover={{scale: 1.1}}>
+      <motion.button whileHover={{scale: 1.1}} whileTap={{ scale: 1.25}}>
         <a href="">
-          <div className='bg-[#D2C1BA] m-10 p-5 rounded-xl font-semibold text-3xl font-["josefin_sans"]'>GET IT ON SHOPIFY</div>
+          <motion.div className='bg-[#D2C1BA] m-10 p-5 rounded-xl font-semibold text-3xl font-["josefin_sans"]'>GET IT ON SHOPIFY</motion.div>
         </a>
       </motion.button>
       <img src={splashphoto} alt='logo' className='md:h-full right-0 fixed top-0 ' />
